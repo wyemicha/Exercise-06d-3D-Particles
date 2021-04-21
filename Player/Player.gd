14b4632b2cc81241c78b19a3201dc14409e15c72
Wyeth <wyemicha@iu.edu>
2021-04-21 00:52:45 -0400

    Completed Ex 06-3D Particles

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody
 
 onready var Camera = $Pivot/Cameraz
-
+onready var Explosion = load("res://Explosion/Explosion.tscn")
+onready var Explosions = get_node("/root/Game/Explosions")
 var gravity = -30
 var max_speed = 8
 var mouse_sensitivity = 0.002
@@ -23,4 +24,8 @@ func _physics_process(_delta):
 		if $Pivot/RayCast.is_colliding():
 			var target = $Pivot/RayCast.get_collider()
 			if target.is_in_group("target"):
+				
+				var explosion = Explosion.instance()
+				Explosions.add_child(explosion)
+				explosion.global_transform.origin = $Pivot/RayCast.get_collision_point()
 				target.die()
